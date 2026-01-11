08b255dc5f5d749271aa50310b3c88b67cdfdcc4
emmanuelist <emmanuel.paul75@yahoo.com>
2026-01-11 22:42:29 +0100

    feat: add Jest configuration for TypeScript with ESM support

---
+++ chrono-vault-frontend/jest.config.mjs
@@ -0,0 +1,16 @@
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', { useESM: true }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  globals: {
+    'ts-jest': {
+      useESM: true,
+      tsconfig: 'tsconfig.json',
+    },
+  },
+};
